118f773c3ca626c1ede8028f3ce016a1c2fe9eb7
Fauzan <fauzanfarooqui7@gmail.com>
2021-04-06 01:22:48 +0530

    Added interactivity feature - git username

--- test.c
+++ test.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main()
 {
-  printf("Hello, world!");
-  
+  char username[10];
+  printf("Hello, world!\nEnter your git username: ");
+  scanf("%s", &username);
+  printf("\nHello, %s!", username);
+    
   return 0;
 }
